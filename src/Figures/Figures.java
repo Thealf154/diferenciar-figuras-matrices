41c9bd43cbc872c6bf5e4c7995d10e9a75bbea9b
Alfredo Vanegas <thealf154@gmail.com>
2020-11-06 15:03:17 -0600

    Added the data examples

--- src/Figures/Figures.java
+++ src/Figures/Figures.java
@@ -21,7 +21,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.io.FileWriter;
 
 import org.json.simple.JSONArray;
